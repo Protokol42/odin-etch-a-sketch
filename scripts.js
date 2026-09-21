5cff2a5c05aa5a72fe3c2c69c5083b0ad642b01b
Nick Huynh <114318124+Protokol42@users.noreply.github.com>
2026-09-21 12:45:11 -0700

    Add the simple js file for Etch-a-sketch

---
+++ scripts.js
@@ -0,0 +1,48 @@
+const mainContainer = document.querySelector(".mainContainer");
+const gridSizeSlider = document.getElementById("gridSizeSlider");
+let mouseIsDown = false;
+createGrid(gridSizeSlider.value);
+gridSizeSlider.addEventListener("input", (event) => {
+  createGrid(event.target.value);
+});
+function createGrid(gridSize) {
+  mainContainer.replaceChildren();
+  for (let i = 0; i < gridSize; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("parentDiv");
+    for (let j = 0; j < gridSize; j++) {
+      let babyDiv = document.createElement("div");
+      babyDiv.dataset.hovered = "false";
+      babyDiv.classList.add("childDiv");
+      newDiv.appendChild(babyDiv);
+    }
+    mainContainer.appendChild(newDiv);
+  }
+}
+
+mainContainer.addEventListener("mouseover", (event) => {
+  let target = event.target;
+  if (!target.classList.contains("childDiv")) return;
+  if (target.dataset.hovered === "true") return;
+  if (mouseIsDown) {
+    target.style.setProperty("background-color", "black");
+    let currentOpacity = parseFloat(target.style.opacity) || 0;
+
+    if (currentOpacity < 1) {
+      currentOpacity += 0.1;
+      target.style.opacity = currentOpacity;
+    }
+
+    if (currentOpacity >= 1) {
+      target.dataset.hovered = "true";
+    }
+  }
+});
+
+mainContainer.addEventListener("mousedown", (event) => {
+  mouseIsDown = true;
+});
+
+window.addEventListener("mouseup", (event) => {
+  mouseIsDown = false;
+});
